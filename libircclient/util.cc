0193c671d381e5b88ec31b27dc5627627c93fea7
Lukas Niederbremer <Lukas.Niederbremer@hs-osnabrueck.de>
2014-09-12 17:52:14 +0200

    Relicense libraries under LGPL

--- libircclient/util.cc
+++ libircclient/util.cc
@@ -1,20 +1,20 @@
 /*
  * Copyright 2014 Lukas Niederbremer
  *
- * This file is part of Luna++.
+ * This file is part of libircclient.
  *
- * Luna++ is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * libircclient is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
  *
- * Luna++ is distributed in the hope that it will be useful,
+ * libircclient is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Luna++.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with libircclient.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "util.h"
